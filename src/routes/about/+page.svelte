df3aff4dcba18c600f8908cadeec8a3032a1df91
BlankParticle <blankparticle@gmail.com>
2023-08-12 23:59:11 +0530

    💠 feat: About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,59 @@
-<h1 class="text-4xl font-heading text-center font-semibold">About</h1>
+<main class="mx-auto w-full max-w-4xl flex flex-col px-4 text-lg">
+  <h1 class="text-4xl font-heading font-bold my-2">About Me</h1>
+  <p>
+    Hello, I am <span class="font-medium">Rahul Mishra</span>, I go by the name
+    <span class="text-brand font-medium">BlankParticle</span>
+    on internet, I am from India and I am {new Date().getFullYear() - 2005} years old.
+  </p>
+  <p>
+    I am doing <span class="font-medium text-brand">B. Tech</span> in
+    <span class="font-medium text-brand">Computer Science</span> from a college in India.
+  </p>
+  <br />
+  <p>
+    I taught myself <span class="font-medium text-brand">Web Development</span> as I had nothing to
+    do during COVID-19 pandemic. My Web Development journey started with PHP while I tried to create
+    <a href="https://www.pocketmine.net/" class="text-brand underline font-medium">PocketMine</a>
+    Plugins for Minecraft (I Know its wired but believe me lol), through that I was exposed to Web Development
+    and did a few things with PHP and MySQL on my phone (Yes, I did it on my phone, thanks to
+    <a href="https://termux.dev/en/" class="text-brand underline font-medium">Termux</a> and
+    <a href="https://acode.foxdebug.com/" class="text-brand underline font-medium">Acode</a>).
+  </p>
+  <p>
+    Later I Learnt <span class="font-medium text-brand">JavaScript/Node.js</span> while trying to
+    make Discord Bots on
+    <a class="text-brand underline font-medium" href="https://glitch.com/">Glitch</a>
+    and <a class="text-brand underline font-medium" href="https://replit.com/">Replit</a>. I copied
+    a Discord Bot template and tweaked around it while I had no clue about JavaScript and learnt
+    most of my JavaScript fundamentals while doing all of this.
+  </p>
+  <br />
+  <p>
+    Enough of my Past, Now days I am learning
+    <a href="https://www.rust-lang.org/" class="font-medium underline text-brand">Rust</a> and
+    Mostly use
+    <a href="https://kit.svelte.dev/" class="font-medium underline text-brand">SvelteKit</a> for my
+    Web Development needs. I have learnt
+    <a href="https://react.dev/" class="font-medium underline text-brand">React</a> and
+    <a href="https://nextjs.org/" class="font-medium underline text-brand">Next.js</a>, but I prefer
+    Svelte.
+  </p>
+  <br />
+  <p>
+    I am still exploring most of new and shiny Tech and I am not sure what I will be working on in
+    future, but I am sure that I will be working on something cool.
+  </p>
+  <h2 class="my-2 text-3xl font-heading font-bold">FAQs</h2>
+  <ul class="list-disc px-4">
+    <li class="my-2">
+      <span class="block font-semibold">Why BlankParticle?</span>
+      <span>
+        To be fair I forgot why I chose my name to be BlankParticle, but It is what it is now.
+      </span>
+    </li>
+    <li class="my-2">
+      <span class="block font-semibold">What do I do beside Programming?</span>
+      <span> I love to watch anime, also I am a huge fan of Linux and Open Source.</span>
+    </li>
+  </ul>
+</main>
